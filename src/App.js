132e68800dd56040c8fd67f4d1dd94eb0ffb938d
Thor Hlavaty <thorthebore@gmail.com>
2020-10-23 15:44:55 -0400

    initial kermit

--- src/App.js
+++ src/App.js
@@ -1,23 +1,28 @@
-import logo from './logo.svg';
-import './App.css';
+import logo from "./logo.svg";
+import "./App.css";
+import Greeter from "./components/Greeter";
+
+const names = ["Nelson", "Atom", "Cici"];
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
+        <p className="greeting"> Hello! </p>
+        {names.map((name, index) => {
+          return <Greeter key={index} name={name} />;
+        })}
+        <Greeter name="Ted" />{" "}
         <a
           className="App-link"
           href="https://reactjs.org"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
-        </a>
-      </header>
+          Learn React{" "}
+        </a>{" "}
+      </header>{" "}
     </div>
   );
 }
---
+++ src/components/Greeter.js
@@ -0,0 +1,9 @@
+import React from "react";
+
+class Greeter extends React.Component {
+  render() {
+    return <h1>Hello, {this.props.name ? this.props.name : "you"}!</h1>;
+  }
+}
+
+export default Greeter;
